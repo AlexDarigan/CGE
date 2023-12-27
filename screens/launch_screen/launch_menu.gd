74505f8247c5697497c794f9f58650346b95caff
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-12-27 00:36:09 +0000

    Start

---
+++ screens/launch_screen/launch_menu.gd
@@ -0,0 +1,28 @@
+extends VBoxContainer
+
+func _on_design_game_pressed():
+	pass # Replace with function body.
+
+
+func _on_host_game_pressed():
+	pass # Replace with function body.
+
+
+func _on_join_game_pressed():
+	pass # Replace with function body.
+
+
+func _on_edit_deck_pressed():
+	pass # Replace with function body.
+
+
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_user_profile_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	pass # Replace with function body.
---
+++ screens/launch_screen/launch_screen.gd
@@ -0,0 +1,22 @@
+extends Control
+
+func _on_design_game_pressed():
+	pass # Replace with function body.
+
+func _on_host_game_pressed():
+	pass # Replace with function body.
+
+func _on_join_game_pressed():
+	pass # Replace with function body.
+
+func _on_edit_deck_pressed():
+	pass # Replace with function body.
+
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+func _on_user_profile_pressed():
+	pass # Replace with function body.
+
+func _on_quit_pressed():
+	pass # Replace with function body.
